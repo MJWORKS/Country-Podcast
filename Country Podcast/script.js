466ab5322b3765f5dc201f3451248d6f10e13ca9
Marcos Júnior Passarella Naves <mjworks2020@outlook.com>
2021-06-07 13:15:11 -0300

    Add files via upload

---
+++ Country Podcast/script.js
@@ -0,0 +1,26 @@
+let audio = document.getElementById("audio1");
+
+function retroceder(){
+    audio.currentTime -= 15; 
+}
+
+function avancar(){
+    audio.currentTime += 15;
+}
+
+function diminuir_vel(){
+    audio.playbackRate -= 0.1;
+}
+
+function aumentar_vel(){
+    audio.playbackRate += 0.1;
+}
+
+function play(){
+    audio.play();
+}
+
+function stop(){
+    audio.pause();
+    audio.currentTime = 0;
+}
